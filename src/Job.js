d7a96ae9481d95681373b9eb481dae5b251cbf3e
kanatamon <ponde.moonlord@gmail.com>
2021-01-23 01:29:44 +0700

    feat: add desktop layout

--- src/Job.js
+++ src/Job.js
@@ -4,19 +4,21 @@ function Job({ data, onLangClick }) {
   return (
     <div className={`Job-card ${data.featured ? '--featured' : ''}`}>
       <img src={data.logo} className="Job-logo" alt="job logo" />
-      <p className="Job-highlight-container">
-        <span className="Job-company">{data.company}</span>
-        {data.new ? <span className="Job-label --new">New!</span> : null}
-        {data.featured ? (
-          <span className="Job-label --featured">Featured</span>
-        ) : null}
-      </p>
-      <p className="Job-position">{data.position}</p>
-      <p className="Job-context-container">
-        <span className="Job-postedAt">{data.postedAt}</span>
-        <span className="Job-contract">{data.contract}</span>
-        <span className="Job-location">{data.location}</span>
-      </p>
+      <div className="Job-details-container">
+        <p className="Job-highlight-container">
+          <span className="Job-company">{data.company}</span>
+          {data.new ? <span className="Job-label --new">New!</span> : null}
+          {data.featured ? (
+            <span className="Job-label --featured">Featured</span>
+          ) : null}
+        </p>
+        <p className="Job-position">{data.position}</p>
+        <p className="Job-context-container">
+          <span className="Job-postedAt">{data.postedAt}</span>
+          <span className="Job-contract">{data.contract}</span>
+          <span className="Job-location">{data.location}</span>
+        </p>
+      </div>
       <hr className="Job-divider" />
       <p className="Job-language-container">
         {data.languages.map((lang) => (
